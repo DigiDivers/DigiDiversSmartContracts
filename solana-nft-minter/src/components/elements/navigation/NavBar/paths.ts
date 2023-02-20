e319cf90ebe61974d795f90b3c0afa9b301f18dc
Emi <andere@uchicago.edu>
2023-02-19 20:07:13 -0600

    adding sol nft creation

---
+++ solana-nft-minter/src/components/elements/navigation/NavBar/paths.ts
@@ -0,0 +1,10 @@
+import { ISubNav } from '../SubNav/SubNav';
+
+const NAV_LINKS: ISubNav[] = [
+  {
+    label: 'NFT Minter',
+    href: '/nftMinter',
+  },
+];
+
+export default NAV_LINKS;
---
+++ solana-nft-minter/src/components/modules/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import { Box, Link, Text } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+
+const links = {
+  github: 'https://github.com/ethereum-boilerplate/ethereum-boilerplate/',
+  forum: 'https://forum.moralis.io/',
+  moralis: 'https://moralis.io/?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplat',
+};
+
+const Footer = () => {
+  return (
+    <Box textAlign={'center'} w="full" p={6}>
+      <Text>
+        Digi Divers NFT Mint
+      </Text>
+    </Box>
+  );
+};
+
+export default Footer;
