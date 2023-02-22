b6c442b3ad3f9e914f5e48d45755fb92d067789b
steven arellano <stevenjarellano2@gmail.com>
2023-02-22 08:51:27 -0600

    linting + check for NFT

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import { checkNFTs } from './checkNFT';
+
 console.log(
 	'this not setup yet. will probably j export everything through here as a module'
 );
+
+async function main() {
+	checkNFTs('EZCZz/y0YImqB6dGtua8GB7lSmZKGr9Go9k7Nsz0CP4=');
+}
+
+main();
