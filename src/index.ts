58c580eaf66218ee92e621118490b0c2fcb29ee2
steven arellano <stevenjarellano2@gmail.com>
2023-02-23 08:51:01 -0600

    send level

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { checkNFTs } from './getNft';
 
 async function main() {
-	// get NFT tests
-	checkNFTs('EZCZz/y0YImqB6dGtua8GB7lSmZKGr9Go9k7Nsz0CP4=');
+    // get NFT tests
+    checkNFTs('EZCZz/y0YImqB6dGtua8GB7lSmZKGr9Go9k7Nsz0CP4=');
 }
 
 main();
